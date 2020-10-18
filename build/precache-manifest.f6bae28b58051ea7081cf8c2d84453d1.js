8aa83e0e3616cf3726ecc0faa5d09587526beca1
cccccyy <1518856229@qq.com>
2020-10-18 19:28:27 +0800

    Signed-off-by: cccccyy <1518856229@qq.com>

---
+++ build/precache-manifest.f6bae28b58051ea7081cf8c2d84453d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "487b4dd15ca2672326b35677c86c2e31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "773d5839efb6dee6b461",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "bccca77ebf9ca7919932",
+    "url": "/static/css/main.a3a8b971.chunk.css"
+  },
+  {
+    "revision": "773d5839efb6dee6b461",
+    "url": "/static/js/2.fa76fac0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fa76fac0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bccca77ebf9ca7919932",
+    "url": "/static/js/main.7d45c2b0.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
